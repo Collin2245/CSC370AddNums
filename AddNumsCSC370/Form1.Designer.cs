f11a6d38dbb06818aaa584332a88e30a2741d0f6
Collin2245 <49080329+Collin2245@users.noreply.github.com>
2020-05-09 18:11:08 -0500

    Final code changes

--- AddNumsCSC370/Form1.Designer.cs
+++ AddNumsCSC370/Form1.Designer.cs
@@ -40,12 +40,28 @@
             this.undoButton = new System.Windows.Forms.Button();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.enterShimWeightInTheEnterValueColumnThenClickAddToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.ErrorBox = new System.Windows.Forms.Label();
             this.count = new System.Windows.Forms.Label();
             this.oooo = new System.Windows.Forms.Label();
             this.saveButton = new System.Windows.Forms.Button();
             this.printButton = new System.Windows.Forms.Button();
-            this.enterShimWeightInTheEnterValueColumnThenClickAddToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label4 = new System.Windows.Forms.Label();
+            this.avgWeight = new System.Windows.Forms.Label();
+            this.maxShim = new System.Windows.Forms.Label();
+            this.minShim = new System.Windows.Forms.Label();
+            this.maxShimWeight = new System.Windows.Forms.Label();
+            this.minShimWeight = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.stDev = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.goalWeightInput = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.goalWeightLabel = new System.Windows.Forms.Label();
+            this.varWeight = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -62,7 +78,7 @@
             // 
             this.InputBox.Location = new System.Drawing.Point(144, 87);
             this.InputBox.Name = "InputBox";
-            this.InputBox.Size = new System.Drawing.Size(122, 20);
+            this.InputBox.Size = new System.Drawing.Size(58, 20);
             this.InputBox.TabIndex = 1;
             // 
             // button1
@@ -78,7 +94,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(73, 184);
+            this.label3.Location = new System.Drawing.Point(73, 169);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(97, 13);
             this.label3.TabIndex = 4;
@@ -88,7 +104,7 @@
             // TotalBox
             // 
             this.TotalBox.AutoSize = true;
-            this.TotalBox.Location = new System.Drawing.Point(177, 213);
+            this.TotalBox.Location = new System.Drawing.Point(177, 182);
             this.TotalBox.Name = "TotalBox";
             this.TotalBox.Size = new System.Drawing.Size(13, 13);
             this.TotalBox.TabIndex = 6;
@@ -97,7 +113,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(73, 213);
+            this.label2.Location = new System.Drawing.Point(73, 182);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(73, 13);
             this.label2.TabIndex = 7;
@@ -106,7 +122,7 @@
             // PrevBox
             // 
             this.PrevBox.AutoSize = true;
-            this.PrevBox.Location = new System.Drawing.Point(177, 184);
+            this.PrevBox.Location = new System.Drawing.Point(177, 169);
             this.PrevBox.Name = "PrevBox";
             this.PrevBox.Size = new System.Drawing.Size(13, 13);
             this.PrevBox.TabIndex = 8;
@@ -125,7 +141,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(236, 208);
+            this.button2.Location = new System.Drawing.Point(298, 198);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(45, 23);
             this.button2.TabIndex = 10;
@@ -135,7 +151,7 @@
             // 
             // undoButton
             // 
-            this.undoButton.Location = new System.Drawing.Point(236, 179);
+            this.undoButton.Location = new System.Drawing.Point(298, 169);
             this.undoButton.Name = "undoButton";
             this.undoButton.Size = new System.Drawing.Size(45, 23);
             this.undoButton.TabIndex = 11;
@@ -147,7 +163,8 @@
             // 
             this.menuStrip1.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.helpToolStripMenuItem});
+            this.helpToolStripMenuItem,
+            this.quitToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Size = new System.Drawing.Size(343, 24);
@@ -157,19 +174,28 @@
             // helpToolStripMenuItem
             // 
             this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.enterShimWeightInTheEnterValueColumnThenClickAddToolStripMenuItem});
+            this.enterShimWeightInTheEnterValueColumnThenClickAddToolStripMenuItem,
+            this.aboutToolStripMenuItem});
             this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
             this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
             this.helpToolStripMenuItem.Text = "Help";
+            this.helpToolStripMenuItem.Click += new System.EventHandler(this.helpToolStripMenuItem_Click);
+            // 
+            // enterShimWeightInTheEnterValueColumnThenClickAddToolStripMenuItem
+            // 
+            this.enterShimWeightInTheEnterValueColumnThenClickAddToolStripMenuItem.Name = "enterShimWeightInTheEnterValueColumnThenClickAddToolStripMenuItem";
+            this.enterShimWeightInTheEnterValueColumnThenClickAddToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.enterShimWeightInTheEnterValueColumnThenClickAddToolStripMenuItem.Text = "Instructions";
+            this.enterShimWeightInTheEnterValueColumnThenClickAddToolStripMenuItem.Click += new System.EventHandler(this.enterShimWeightInTheEnterValueColumnThenClickAddToolStripMenuItem_Click);
             // 
             // ErrorBox
             // 
             this.ErrorBox.AutoSize = true;
-            this.ErrorBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ErrorBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
             this.ErrorBox.ForeColor = System.Drawing.Color.Red;
-            this.ErrorBox.Location = new System.Drawing.Point(93, 235);
+            this.ErrorBox.Location = new System.Drawing.Point(141, 65);
             this.ErrorBox.Name = "ErrorBox";
-            this.ErrorBox.Size = new System.Drawing.Size(0, 25);
+            this.ErrorBox.Size = new System.Drawing.Size(0, 13);
             this.ErrorBox.TabIndex = 13;
             // 
             // count
@@ -192,7 +218,7 @@
             // 
             // saveButton
             // 
-            this.saveButton.Location = new System.Drawing.Point(0, 256);
+            this.saveButton.Location = new System.Drawing.Point(298, 227);
             this.saveButton.Name = "saveButton";
             this.saveButton.Size = new System.Drawing.Size(45, 23);
             this.saveButton.TabIndex = 16;
@@ -210,12 +236,147 @@
             this.printButton.UseVisualStyleBackColor = true;
             this.printButton.Click += new System.EventHandler(this.printButton_Click);
             // 
-            // enterShimWeightInTheEnterValueColumnThenClickAddToolStripMenuItem
+            // aboutToolStripMenuItem
             // 
-            this.enterShimWeightInTheEnterValueColumnThenClickAddToolStripMenuItem.Name = "enterShimWeightInTheEnterValueColumnThenClickAddToolStripMenuItem";
-            this.enterShimWeightInTheEnterValueColumnThenClickAddToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.enterShimWeightInTheEnterValueColumnThenClickAddToolStripMenuItem.Text = "Instructions";
-            this.enterShimWeightInTheEnterValueColumnThenClickAddToolStripMenuItem.Click += new System.EventHandler(this.enterShimWeightInTheEnterValueColumnThenClickAddToolStripMenuItem_Click);
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(42, 20);
+            this.quitToolStripMenuItem.Text = "Quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(73, 195);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Avg. shim weight:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // avgWeight
+            // 
+            this.avgWeight.AutoSize = true;
+            this.avgWeight.Location = new System.Drawing.Point(177, 195);
+            this.avgWeight.Name = "avgWeight";
+            this.avgWeight.Size = new System.Drawing.Size(13, 13);
+            this.avgWeight.TabIndex = 19;
+            this.avgWeight.Text = "0";
+            // 
+            // maxShim
+            // 
+            this.maxShim.AutoSize = true;
+            this.maxShim.Location = new System.Drawing.Point(73, 208);
+            this.maxShim.Name = "maxShim";
+            this.maxShim.Size = new System.Drawing.Size(88, 13);
+            this.maxShim.TabIndex = 20;
+            this.maxShim.Text = "Max shim weight:";
+            this.maxShim.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // minShim
+            // 
+            this.minShim.AutoSize = true;
+            this.minShim.Location = new System.Drawing.Point(73, 221);
+            this.minShim.Name = "minShim";
+            this.minShim.Size = new System.Drawing.Size(85, 13);
+            this.minShim.TabIndex = 21;
+            this.minShim.Text = "Min shim weight:";
+            // 
+            // maxShimWeight
+            // 
+            this.maxShimWeight.AutoSize = true;
+            this.maxShimWeight.Location = new System.Drawing.Point(177, 208);
+            this.maxShimWeight.Name = "maxShimWeight";
+            this.maxShimWeight.Size = new System.Drawing.Size(13, 13);
+            this.maxShimWeight.TabIndex = 22;
+            this.maxShimWeight.Text = "0";
+            // 
+            // minShimWeight
+            // 
+            this.minShimWeight.AutoSize = true;
+            this.minShimWeight.Location = new System.Drawing.Point(177, 221);
+            this.minShimWeight.Name = "minShimWeight";
+            this.minShimWeight.Size = new System.Drawing.Size(13, 13);
+            this.minShimWeight.TabIndex = 23;
+            this.minShimWeight.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(73, 234);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(99, 13);
+            this.label6.TabIndex = 24;
+            this.label6.Text = "Standard deviation:";
+            // 
+            // stDev
+            // 
+            this.stDev.AutoSize = true;
+            this.stDev.Location = new System.Drawing.Point(177, 234);
+            this.stDev.Name = "stDev";
+            this.stDev.Size = new System.Drawing.Size(13, 13);
+            this.stDev.TabIndex = 25;
+            this.stDev.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(207, 90);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(66, 13);
+            this.label7.TabIndex = 26;
+            this.label7.Text = "Goal weight:";
+            // 
+            // goalWeightInput
+            // 
+            this.goalWeightInput.Location = new System.Drawing.Point(279, 87);
+            this.goalWeightInput.Name = "goalWeightInput";
+            this.goalWeightInput.Size = new System.Drawing.Size(51, 20);
+            this.goalWeightInput.TabIndex = 27;
+            this.goalWeightInput.Text = "100";
+            this.goalWeightInput.TextChanged += new System.EventHandler(this.goalWeightInput_TextChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(73, 247);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(91, 13);
+            this.label8.TabIndex = 28;
+            this.label8.Text = "Amount until goal:";
+            // 
+            // goalWeightLabel
+            // 
+            this.goalWeightLabel.AutoSize = true;
+            this.goalWeightLabel.Location = new System.Drawing.Point(177, 247);
+            this.goalWeightLabel.Name = "goalWeightLabel";
+            this.goalWeightLabel.Size = new System.Drawing.Size(25, 13);
+            this.goalWeightLabel.TabIndex = 29;
+            this.goalWeightLabel.Text = "100";
+            // 
+            // varWeight
+            // 
+            this.varWeight.Location = new System.Drawing.Point(12, 201);
+            this.varWeight.Name = "varWeight";
+            this.varWeight.Size = new System.Drawing.Size(52, 20);
+            this.varWeight.TabIndex = 30;
+            this.varWeight.Text = "5";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 221);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(52, 46);
+            this.button3.TabIndex = 31;
+            this.button3.Text = "Click to add";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // OutputBox
             // 
@@ -223,6 +384,20 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(343, 279);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.varWeight);
+            this.Controls.Add(this.goalWeightLabel);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.goalWeightInput);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.stDev);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.minShimWeight);
+            this.Controls.Add(this.maxShimWeight);
+            this.Controls.Add(this.minShim);
+            this.Controls.Add(this.maxShim);
+            this.Controls.Add(this.avgWeight);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.printButton);
             this.Controls.Add(this.saveButton);
             this.Controls.Add(this.count);
@@ -270,6 +445,22 @@
         private System.Windows.Forms.ToolStripMenuItem enterShimWeightInTheEnterValueColumnThenClickAddToolStripMenuItem;
         private System.Windows.Forms.Button saveButton;
         private System.Windows.Forms.Button printButton;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label avgWeight;
+        private System.Windows.Forms.Label maxShim;
+        private System.Windows.Forms.Label minShim;
+        private System.Windows.Forms.Label maxShimWeight;
+        private System.Windows.Forms.Label minShimWeight;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label stDev;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox goalWeightInput;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label goalWeightLabel;
+        private System.Windows.Forms.TextBox varWeight;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- AddNumsCSC370/Form1.cs
+++ AddNumsCSC370/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace AddNumsCSC370
 {
     public partial class OutputBox : Form
@@ -22,9 +23,10 @@ namespace AddNumsCSC370
             InputBox.Focus();
         }
 
+
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            this.Text = "Shim calculator";
         }
 
         //When you click, this adds to a previous num, as well as a current running total. This is done to keep track of what has just been done, and a sum of all of thats been done
@@ -42,7 +44,15 @@ namespace AddNumsCSC370
                 countNum += 1;
                 count.Text = countNum.ToString();
                 ErrorBox.Text = "";
-                
+                avgWeight.Text = allEntered.Average().ToString();
+                minShimWeight.Text = allEntered.Min().ToString();
+                maxShimWeight.Text = allEntered.Max().ToString();
+                double average = allEntered.Average();
+                double sumOfSquaresOfDifferences = allEntered.Select(val => (val - average) * (val - average)).Sum();
+                double sd = Math.Sqrt(sumOfSquaresOfDifferences / allEntered.Count);
+                stDev.Text = sd.ToString();
+                try { goalWeightLabel.Text = (Convert.ToDouble(goalWeightInput.Text) - allEntered.Sum()).ToString(); } catch { goalWeightLabel.Text = "Error goal"; }
+
             }
             catch
             {
@@ -67,6 +77,11 @@ namespace AddNumsCSC370
             allEntered.Clear();
             countNum = 0;
             count.Text = "0";
+            avgWeight.Text = "0";
+            minShimWeight.Text = "0";
+            maxShimWeight.Text = "0";
+            stDev.Text = "0";
+            try { goalWeightLabel.Text = (Convert.ToDouble(goalWeightInput.Text) - allEntered.Sum()).ToString(); } catch { goalWeightLabel.Text = "Error goal"; }
         }
         //This creates a list with all inputs, and can be clicked as many times as it has been entered.
         private void undoButton_Click(object sender, EventArgs e)
@@ -78,20 +93,37 @@ namespace AddNumsCSC370
                 TotalBox.Text = runningTotal.ToString(); //sets new total
                 countNum -= 1;
                 count.Text = countNum.ToString();
+                
                 if (allEntered.Count() > 0)
                 {
                     PrevBox.Text = allEntered[allEntered.Count() - 1].ToString(); //sets prev number
+                    avgWeight.Text = allEntered.Average().ToString();
+                    minShimWeight.Text = allEntered.Min().ToString();
+                    maxShimWeight.Text = allEntered.Max().ToString();
+                    double average = allEntered.Average();
+                    double sumOfSquaresOfDifferences = allEntered.Select(val => (val - average) * (val - average)).Sum();
+                    double sd = Math.Sqrt(sumOfSquaresOfDifferences / allEntered.Count);
+                    stDev.Text = sd.ToString();
+                    //goalWeightLabel.Text = 
+                    try { goalWeightLabel.Text= (Convert.ToDouble(goalWeightInput.Text) - allEntered.Sum()).ToString(); } catch { goalWeightLabel.Text = "Error goal"; }
+
                 }
                 else
                 {
                     PrevBox.Text = "0";
+                    avgWeight.Text = "0";
+                    minShimWeight.Text = "0";
+                    maxShimWeight.Text = "0";
+                    stDev.Text = "0";
+                    try { goalWeightLabel.Text = (Convert.ToDouble(goalWeightInput.Text) - allEntered.Sum()).ToString(); } catch { goalWeightLabel.Text = "Error goal"; }
                 }
             }
+            
         }
 
         private void enterShimWeightInTheEnterValueColumnThenClickAddToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Enter input in text box. Click button to add nums. Undo removes previous input and clear removes all. Save saves to file, and print displays current weight and count and clears current total.  ");
+            MessageBox.Show("Enter input in text box. Click button to add nums. Undo removes previous input and clear removes all. Save saves to file, and print displays current weight and count and clears current total. Enter a goal to aim for for the day. also, the add variable weight adds what the user sets on button click. ");
                   
         }
         //This opens up a pop up box with current info
@@ -105,13 +137,83 @@ namespace AddNumsCSC370
             allEntered.Clear();
             countNum = 0;
             count.Text = "0";
+            avgWeight.Text = "0";
+            minShimWeight.Text = "0";
+            maxShimWeight.Text = "0";
+            stDev.Text = "0";
+            try { goalWeightLabel.Text = (Convert.ToDouble(goalWeightInput.Text) - allEntered.Sum()).ToString(); } catch { goalWeightLabel.Text = "Error goal"; }
+
         }
         //This prints the current info to C:\Users\Public/WriteText.txt and overwrites every time 
         private void saveButton_Click(object sender, EventArgs e)
         {
             DateTime now = DateTime.Now;
-            string text = "Date: "  + now + "\n" + "Total weight: " + runningTotal +"\n"+ "Count of shims: " + countNum + "\n" + "Avg weight of shim: " + runningTotal/countNum;
-            System.IO.File.WriteAllText(@"C:\Users\Public/WriteText.txt", text);
+            string text = "Date: "  + now + "\n" + "Total weight: " + runningTotal +"\n"+ "Count of shims: " + countNum + "\n" + "Avg weight of shim: " + runningTotal/countNum
+                +"\nAmount left for goal: " +goalWeightLabel.Text;
+            System.IO.File.WriteAllText(@"C:\Users\Public/AddNums.txt", text);
+            MessageBox.Show("File written to "+@"C:/Users/Public/AddNums.txt");
+        }
+
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This program is made for steel workers as a way to easily add shims to their weight totals. For any help please contact Collin.Krueger@cuw.edu.");
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void goalWeightInput_TextChanged(object sender, EventArgs e)
+        {
+            try { goalWeightLabel.Text = (Convert.ToDouble(goalWeightInput.Text) - allEntered.Sum()).ToString(); } catch { goalWeightLabel.Text = "Error goal"; }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                input = Convert.ToDouble(varWeight.Text);
+                this.InputBox.Text = "";
+                double output = input;
+                runningTotal += input;
+                PrevBox.Text = Convert.ToString(output);
+                TotalBox.Text = Convert.ToString(runningTotal);
+                allEntered.Add(input);
+                countNum += 1;
+                count.Text = countNum.ToString();
+                ErrorBox.Text = "";
+                avgWeight.Text = allEntered.Average().ToString();
+                minShimWeight.Text = allEntered.Min().ToString();
+                maxShimWeight.Text = allEntered.Max().ToString();
+                double average = allEntered.Average();
+                double sumOfSquaresOfDifferences = allEntered.Select(val => (val - average) * (val - average)).Sum();
+                double sd = Math.Sqrt(sumOfSquaresOfDifferences / allEntered.Count);
+                stDev.Text = sd.ToString();
+                try { goalWeightLabel.Text = (Convert.ToDouble(goalWeightInput.Text) - allEntered.Sum()).ToString(); } catch { goalWeightLabel.Text = "Error goal"; }
+
+            }
+            catch
+            {
+
+                ErrorBox.Text = "Incorrect input";
+
+            }
         }
     }
 }
